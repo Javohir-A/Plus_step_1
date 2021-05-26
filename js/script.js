f0ae7363fbb60f1a06f0413552cdb59bd49d0f85
Javohir <abdusamatovjavohir@gmail.com>
2021-05-26 07:22:06 +0500

    add mobile verst

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/* var name="Javohir";
+
+let numbber = 7;
+const pi =  3.14;
+
+
+numbber = 5;
+
+let leftBorderWidth = 200;
+
+console.log(numbber);
+let ansewr = prompt("Вам есть 18?");
+console.log(ansewr); 
+
+ console.log(4-2); 
+ let isChecked =true,
+    isClose = false; 
+
+console.log (isChecked && isClose);
+ 
+console.log (isChecked || isClose); */
+
+/* let ansewr = confirm("Вам есть 18?")
+if(ansewr) {
+    console.log("Kiring")
+}else{
+    console.log("CHIQ NAXUY")
+} */
+
+/* for(let i = 1; i < 8; i++ ){
+    console.log(i);
+}
+ */
+function logging(a, b){
+    console.log(a + b)
+}
+
+logging(5, 8);
